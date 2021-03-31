a889c1830b32c59fe840e8133488101be9cc01d3
Hemantshetty <81672261+Hemantshetty@users.noreply.github.com>
2021-03-31 12:07:54 +0530

    Add files via upload

---
+++ nCr.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+int fact(int n)
+{
+	static int a;
+	if(n>0)
+	{
+		a=n*fact(n-1);
+	}
+	else
+	{
+		return 1;
+	}
+	return	a;
+}
+float main() 
+{
+	int n,r;
+	float ncr;
+	printf("Enter N: ");
+	scanf("%d",&n);
+	printf("Enter R: ");
+	scanf("%d",&r);
+	if(r>0 && r<=n)
+	{
+		ncr=fact(n)/fact(r)*fact(n-r);
+		printf("%f",ncr);
+	}
+	else
+	{
+		printf("Invalid Input");
+	}
+	return 0;
+}
