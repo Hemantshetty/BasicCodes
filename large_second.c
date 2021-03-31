14bcdb10de2fc6aa976981e17738d8dea5f07f61
Hemantshetty <81672261+Hemantshetty@users.noreply.github.com>
2021-03-31 12:27:22 +0530

    Add files via upload
    Fing the largest and second largest element from the array using C language

---
+++ large_second.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+	int n,i,large,second;
+	printf("Enter the size of array N: ");
+	scanf("%d",&n);
+	int a[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	large=a[1];
+	second=a[0];
+	
+	for(i=0;i<n;i++)
+	{
+		if(	a[i]>large)
+		{
+			second=large;
+			large=a[i];
+		}
+		else if (a[i]>second && a[i]!=large)
+		{
+			second=a[i];
+		}
+	}
+	
+		printf("Largest no is:%d\nSecond largest no is: %d",large,second);
+	return 0;
+}
