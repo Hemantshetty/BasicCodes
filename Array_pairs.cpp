e9a922a1e017d8c2e5def4f0837e26460ad8b771
Hemantshetty <81672261+Hemantshetty@users.noreply.github.com>
2021-03-31 12:22:10 +0530

    Add files via upload

---
+++ Array_pairs.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Generate the pairs of user defined array and find the sum of each pairs
+
+int main() 
+{
+	int i,j,n,s[10][10];
+	scanf("%d",&n);
+	int a[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			printf("(%d,%d) ",a[i],a[j]);
+			s[i][j]=a[i]+a[j];
+			printf("Sum=%d \n",s[i][j]);
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
